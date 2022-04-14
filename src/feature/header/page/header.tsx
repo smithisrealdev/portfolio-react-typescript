c5bb88ee794fea05e5501f092bcb60cf31cd160f
apichet.nua <apichet.nua@msu.ac.th>
2022-04-14 23:42:40 +0700

    header bar

--- src/feature/header/page/header.tsx
+++ src/feature/header/page/header.tsx
@@ -1,45 +1,136 @@
-import React from 'react'
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid';
 import SwitchDarkMode from '../components/switch_dark_mode'
 import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
 import SwitchChangeLang from '../components/switch_change_lang'
 import { onEnter, onLeave } from '../../../shared/components/animation/withHoverText'
 import { FormatMessage } from '../../change_lang/change'
-export default function header() {
+
+
+const pages = [
+    {
+        page: 'Home',
+        route: '/'
+    },
+    {
+        page: 'header.about',
+        route: 'about'
+    },
+    {
+        page: 'header.skill',
+        route: 'skill'
+    },
+    {
+        page: 'header.experiance',
+        route: 'experiance'
+    },
+    {
+        page: 'header.education',
+        route: 'education'
+    },
+    {
+        page: 'header.contact',
+        route: 'contact'
+    }
+];
+
+const header = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
-        <Box className="sticky top-0 z-50">
-            <nav className='flex pl-20 pt-5 m-0 w-full h-full text-white justify-between items-center border-x-inherit bg-main' >
-                <div className='font-semibold font-atma' onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                    <Link to="/">SmiTH</Link>
-                </div>
-                <div className='flex w-2/5 justify-between font-semibold font-atma'>
-                    {/* <h4>about</h4> */}
-                    <Box onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                        <Link to="about">{FormatMessage('header.about')}</Link>
+        <Container maxWidth="xl" className='pb-32'>
+            <AppBar position="fixed" >
+                <Toolbar disableGutters >
+                    <Box className='pl-6 pr-6 pt-5 b-36 m-0 w-full h-full text-white justify-between items-center bg-main '>
+                        <Container maxWidth="xl">
+                            <Grid className='flex items-center'>
+                                <Grid
+                                    className='font-atma font-bold'
+                                    sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                                    onMouseEnter={onEnter} onMouseLeave={onLeave}
+                                >
+                                    <Link to="/">SmiTH</Link>
+                                </Grid>
+                                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                                    <IconButton
+                                        size="large"
+                                        aria-label="account of current user"
+                                        aria-controls="menu-appbar"
+                                        aria-haspopup="true"
+                                        onClick={handleOpenNavMenu}
+                                        color="inherit"
+                                    >
+                                        <MenuIcon />
+                                    </IconButton>
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorElNav}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'left',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'left',
+                                        }}
+                                        open={Boolean(anchorElNav)}
+                                        onClose={handleCloseNavMenu}
+                                        sx={{
+                                            display: { xs: 'block', md: 'none' },
+                                        }}
+                                    >
+                                        {pages.map((items) => (
+                                            <MenuItem key={items.page} onClick={handleCloseNavMenu}>
+                                                <Link to={items.route}>{FormatMessage(items.page)}</Link>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </Box>
+                                <Box className='flex gap-16 justify-center text-center font-atma font-semibold' sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                    {pages.map((items) => (
+                                    
+                                        <Box
+                                            // onClick={handleCloseNavMenu}
+                                            sx={{ my: 2, color: 'white', display: 'block' }}
+                                            onMouseEnter={onEnter} onMouseLeave={onLeave}
+                                        >
+                                            <Link to={items.route}>{FormatMessage(items.page)}</Link>
+                                        </Box>
+                                    ))}
+                                </Box>
+                                <Box sx={{ flexGrow: 0 }}>
+                                    <Grid className='flex items-center justify-center'>
+                                        <SwitchChangeLang />
+                                        <SwitchDarkMode />
+                                        <h1 className='font-atma text-sm'>
+                                            {FormatMessage('header.version')}
+                                        </h1>
+                                    </Grid>
+                                </Box>
+                            </Grid>
+                        </Container>
                     </Box>
-                    <Box onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                        <Link to="skill">{FormatMessage('header.skill')}</Link>
-                    </Box>
-                    <Box onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                        <Link to="experiance">{FormatMessage('header.experiance')}</Link>
-                    </Box>
-                    <Box onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                        <Link to="education">{FormatMessage('header.education')}</Link>
-                    </Box>
-                    <Box onMouseEnter={onEnter} onMouseLeave={onLeave}>
-                        <Link to="contact">{FormatMessage('header.contact')}</Link>
-                    </Box>
-                </div>
-                <div className='flex w-3/12 items-center justify-center'>
-                    <SwitchChangeLang />
-                    <SwitchDarkMode />
-                    <h1 className='font-atma text-sm'>
-                        {FormatMessage('header.version')}
-                    </h1>
-
-                </div>
-            </nav>
-        </Box>
-    )
-}
+                </Toolbar>
+            </AppBar>
+        </Container>
+    );
+};
+export default header;
