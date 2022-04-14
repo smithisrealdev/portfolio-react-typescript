dce7c4db25e7f453407664005d9ae0859fbf00cd
apichet.nua <apichet.nua@msu.ac.th>
2022-04-15 00:14:35 +0700

    refactor code

--- src/feature/about_me/components/resume.tsx
+++ src/feature/about_me/components/resume.tsx
@@ -16,37 +16,29 @@ export default function resume() {
         gsap.to(q(".boxBg"), {
             x: 6,
             y: -7,
-            duration: 1,
-            // repeat: 1,
         });
         gsap.to(q(".box2"), {
             x: 17,
             y: -18,
-            duration: 1,
-            // repeat: 1,
         });
     };
     const onLeave = () => {
         gsap.to(q(".boxBg"), {
             x: 0,
             y: 0,
-            duration: 1,
-            // repeat: 1,
         });
         gsap.to(q(".box2"), {
             x: 0,
             y: 0,
-            duration: 1,
-            // repeat: 1,
         });
     };
 
     return (
         <React.Fragment>
             <div className=' z-20' ref={el}>
-                <div className=' w-24 shadow-2xl bg-red-100  h-12 resume-custom -z-0 ' />
-                <div className='boxBg -mt-12 w-24 bg-red-500 h-12 ' />
-                <div onMouseEnter={onEnter} onClick={()=> setOpen(true) } onMouseLeave={onLeave} className='box2 -mt-12 w-24 bg-resume h-12 items-center justify-center flex cursor-pointer ' >
+                <div className=' w-24 shadow-2xl bg-indigo-100  h-12 resume-custom -z-0 ' />
+                <div className='boxBg -mt-12 w-24 bg-indigo-400 h-12 ' />
+                <div onMouseEnter={onEnter} onClick={()=> setOpen(true) } onMouseLeave={onLeave} className='box2 -mt-12 w-24 bg-indigo-500 h-12 items-center justify-center flex cursor-pointer ' >
                     <Link href={ResumePDF} download underline="none">
                         <h1 className='font-atma text-white font-medium'>
                             {FormatMessage('main.resume')}
